c26606d4b72335f27e3d529e9e64c750d04434b8
Erin Salva <salvaerin@gmail.com>
2020-07-14 11:57:10 -0400

    To Do list complete stage 0

---
+++ ToDoList/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Erin Salva on 7/14/20.
+//  Copyright © 2020 Erin Salva. All rights reserved.
+//
+
+import UIKit
+
+class ToDo{
+    var name = ""
+    var important = false
+}
